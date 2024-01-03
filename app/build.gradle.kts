8d731f6dd0f78ec01141ed3b78098cbbffc53bb4
nghiemkythu <nghiemdt2013@gmail.com>
2024-01-03 18:38:42 +0700

    add imple

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,6 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation ("androidx.navigation:navigation-compose:2.6.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 }
