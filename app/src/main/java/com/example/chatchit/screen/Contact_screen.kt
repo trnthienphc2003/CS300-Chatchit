7c1ff50ea590ff084b18436b4ad6ba82733b8f38
trnthienphc2003 <88192288+trnthienphc2003@users.noreply.github.com>
2024-01-08 09:10:31 +0700

    feat: Add alphabet bar and modify design in contact screen

--- app/src/main/java/com/example/chatchit/screen/Contact_screen.kt
+++ app/src/main/java/com/example/chatchit/screen/Contact_screen.kt
@@ -3,50 +3,78 @@ package com.example.chatchit.screen
 import android.content.Context
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.chatchit.component.IconComponentDrawable
 import com.example.chatchit.component.data.Person
 import com.example.chatchit.component.data.personList
 import com.example.chatchit.models.Room
 import com.example.chatchit.models.User
+import kotlinx.coroutines.launch
+import kotlin.reflect.KProperty
+//import androidx.compose.runtime.getValue
+//import androidx.compose.runtime.livedata.observeAsState
 
 @Composable
 fun ContactScreen(
     navHostController: NavHostController,
-    context: Context
+    context: Context,
 ) {
     val user = navHostController.previousBackStackEntry?.savedStateHandle?.get<Room>("user") ?: User()
+    val listState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
     Column (
         modifier = androidx.compose.ui.Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ){
         Text(
-            text = "Contact Screen",
+            text = "Contacts",
             fontWeight = FontWeight.Bold,
 //            modifier = Modifier.fillMaxSize(),
         )
 
-        ContactList(grouped = grouped, modifier = Modifier)
+        Row(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            AlphabetBar(grouped = grouped) {pos ->
+                coroutineScope.launch{
+                    listState.animateScrollToItem(pos)
+                }
+            }
+            ContactList(grouped = grouped, modifier = Modifier.weight(1f), listState)
+        }
     }
 }
 
@@ -56,11 +84,13 @@ val grouped = personList.sortedBy { it.name } .groupBy { it.name[0] }
 @Composable
 fun ContactList (
     grouped: Map<Char, List<Person>>,
-    modifier: androidx.compose.ui.Modifier
+    modifier: androidx.compose.ui.Modifier,
+    listState: androidx.compose.foundation.lazy.LazyListState
 ) {
     LazyColumn(
-        modifier = androidx.compose.ui.Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+        modifier = modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        state = listState
     ) {
         grouped.forEach { (initial, contactForLetter) ->
             stickyHeader {
@@ -70,6 +100,49 @@ fun ContactList (
             items(items = contactForLetter) { person ->
 //                Items component
                 contactListItem(person = person)
+                if(contactForLetter.indexOf(person) != contactForLetter.lastIndex) {
+                    Divider(
+                        modifier = Modifier.padding(horizontal = 12.dp),
+                        color = Color(0xFFE7E7E7)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AlphabetBar(
+    grouped: Map<Char, List<Person>>,
+    toHeader: (Int) -> Unit
+) {
+    val letterPos = mutableMapOf<Char, Int>()
+    var count by rememberSaveable{ mutableStateOf(0) }
+
+    grouped.forEach { (letter, contactsForLetter) ->
+        letterPos[letter] = count
+        count += contactsForLetter.size + 1
+    }
+
+    Column (
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(horizontal = 6.dp, vertical = 36.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(3.dp),
+
+    ) {
+        for (alphabet in 'A'..'Z') {
+            Box(
+                modifier = Modifier
+                    .clickable { letterPos[alphabet]?.let { toHeader(it) } }
+                    .size(24.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = alphabet.toString(),
+                    textAlign = TextAlign.Center
+                )
             }
         }
     }
@@ -105,10 +178,14 @@ fun contactListItem(person: Person) {
                 .size(36.dp),
             contentAlignment = Alignment.Center
         ) {
-            Text(
-                text = person.name.first().toString(),
-                color = Color.White,
+            IconComponentDrawable(
+                icon = person.icon,
+                size = 36.dp
             )
+//            Text(
+//                text = person.name.first().toString(),
+//                color = Color.White,
+//            )
         }
 
         Text(
@@ -117,4 +194,13 @@ fun contactListItem(person: Person) {
             color = Color.DarkGray
         )
     }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ContactScreenPreview() {
+    ContactScreen(
+        navHostController = NavHostController(androidx.compose.ui.platform.LocalContext.current),
+        context = androidx.compose.ui.platform.LocalContext.current
+    )
 }
