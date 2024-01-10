9bb715edb9026df54e49056902d5d7281332afdf
trnthienphc2003 <88192288+trnthienphc2003@users.noreply.github.com>
2024-01-10 16:49:06 +0700

    feat: Add log out

--- app/src/main/java/com/example/chatchit/screen/Setting_screen.kt
+++ app/src/main/java/com/example/chatchit/screen/Setting_screen.kt
@@ -3,7 +3,10 @@ package com.example.chatchit.screen
 import Avatar
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.SharedPreferences
+import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -40,6 +43,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,10 +52,12 @@ import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavHostController
 import com.example.chatchit.R
 import com.example.chatchit.component.SpacerHeight
+import com.example.chatchit.component.SpacerWidth
 import com.example.chatchit.models.Language
 import com.example.chatchit.models.Room
 import com.example.chatchit.models.User
 import com.example.chatchit.services.APIService
+import com.example.chatchit.services.api.AuthAPI
 import com.example.chatchit.services.api.LanguageAPI
 import com.example.chatchit.services.api.await
 import com.example.chatchit.services.api.form.LanguageIdField
@@ -68,6 +74,7 @@ fun SettingScreen(
     context: Context
 ) {
     var openLanguageLog by rememberSaveable { mutableStateOf(false) }
+    var openSignoutLog by rememberSaveable { mutableStateOf(false) }
     val user = navHostController.previousBackStackEntry?.savedStateHandle?.get<User>("user") ?: User()
     val listOfLanguages = navHostController.previousBackStackEntry?.savedStateHandle?.get<List<Language>>("listOfLanguages") ?: List<Language>(0) { Language() }
 
@@ -152,7 +159,9 @@ fun SettingScreen(
         Spacer(modifier = Modifier.padding(vertical = 24.dp))
 
         Row(
-            modifier = Modifier.fillMaxWidth().padding(start = 24.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 24.dp),
             horizontalArrangement = Arrangement.Absolute.Left
         ) {
             IconButton(onClick = { /*TODO*/ },
@@ -201,7 +210,9 @@ fun SettingScreen(
         Spacer(modifier = Modifier.padding(vertical = 20.dp))
 
         Row(
-            modifier = Modifier.fillMaxWidth().padding(start = 24.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 24.dp),
             horizontalArrangement = Arrangement.Absolute.Left
         ) {
             IconButton(onClick = { /*TODO*/ },
@@ -250,7 +261,8 @@ fun SettingScreen(
 
         Row(
             modifier = Modifier
-                .fillMaxWidth().padding(start = 24.dp),
+                .fillMaxWidth()
+                .padding(start = 24.dp),
             horizontalArrangement = Arrangement.Absolute.Left
         ) {
             IconButton(onClick = { /*TODO*/ },
@@ -299,7 +311,9 @@ fun SettingScreen(
         Spacer(modifier = Modifier.padding(vertical = 20.dp))
 
         Row(
-            modifier = Modifier.fillMaxWidth().padding(start = 24.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 24.dp),
             horizontalArrangement = Arrangement.Absolute.Left
         ) {
             IconButton(onClick = { /*TODO*/ },
@@ -346,7 +360,9 @@ fun SettingScreen(
 
         Spacer(modifier = Modifier.padding(vertical = 20.dp))
         Row(
-            modifier = Modifier.fillMaxWidth().padding(start = 24.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 24.dp),
             horizontalArrangement = Arrangement.Absolute.Left
         ) {
             IconButton(onClick = { /*TODO*/ },
@@ -382,8 +398,13 @@ fun SettingScreen(
 
         Spacer(modifier = Modifier.padding(vertical = 20.dp))
         Row(
-            modifier = Modifier.fillMaxWidth().padding(start = 24.dp),
-            horizontalArrangement = Arrangement.Absolute.Left
+            horizontalArrangement = Arrangement.Absolute.Left,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 24.dp)
+                .clickable {
+                    openSignoutLog = true
+                }
         ) {
             IconButton(onClick = { /*TODO*/ },
                 modifier = Modifier.size(44.dp)) {
@@ -414,7 +435,99 @@ fun SettingScreen(
                         .padding(top = 15.dp),
                 )
             }
+        }
 
+        if(openSignoutLog) {
+            Dialog(onDismissRequest = { openSignoutLog = false }) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp),
+                    shape = RoundedCornerShape(10.dp),
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp),
+                        verticalArrangement = Arrangement.Center,
+//                horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "Sign out",
+                            style = TextStyle(
+                                fontSize = 40.sp,
+                                color = Color.Black,
+                                fontWeight = FontWeight.Bold
+                            ),
+                            textAlign = TextAlign.Left,
+                        )
+                        SpacerHeight(10.dp)
+                        Text(
+                            text = "Are you sure you want to sign out?",
+                            style = TextStyle(
+                                fontSize = 15.sp,
+                                color = Color.Gray,
+                                fontWeight = FontWeight.Bold,
+                            ),
+                            textAlign = TextAlign.Left,
+                        )
+//                SpacerHeight(10.dp)
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 10.dp)
+                                .weight(1f),
+                            horizontalArrangement = Arrangement.End,
+                            verticalAlignment = Alignment.Bottom
+                        ) {
+                            Text(
+                                text = "Cancel",
+                                style = TextStyle(
+                                    fontSize = 20.sp,
+                                    color = Color.Gray,
+                                    fontWeight = FontWeight.Bold
+                                ),
+                                textAlign = TextAlign.Left,
+                                modifier = Modifier
+                                    .clickable {
+                                openSignoutLog = false
+                                    }
+                            )
+                            SpacerWidth(10.dp)
+                            Text(
+                                text = "Sign out",
+                                style = TextStyle(
+                                    fontSize = 20.sp,
+                                    color = Color.Red,
+                                    fontWeight = FontWeight.Bold
+                                ),
+                                textAlign = TextAlign.Left,
+                                modifier = Modifier
+                                    .clickable {
+                                    openSignoutLog = false
+                                            val authService: AuthAPI = APIService.getApiClient(context).create(
+                                                AuthAPI::class.java)
+                                            MainScope().launch {
+                                                try {
+                                                    val response = authService.logout().await()
+                                                    Toast.makeText(context, "Log out successfully!", Toast.LENGTH_SHORT).show()
+                                                    val logoutService: AuthAPI = APIService.getApiClient(context, clearCookie = true).create(
+                                                        AuthAPI::class.java)
+                                                    val cookiePrefs: SharedPreferences = context.getSharedPreferences("Cookies", Context.MODE_PRIVATE)
+                                                    val cookies = ""
+
+                                                    val editor = cookiePrefs.edit()
+                                                } catch (e: Exception) {
+                                                    e.printStackTrace()
+                                                }
+                                            }
+//                                    navHostController.navigate("login")
+                                    }
+                            )
+                        }
+                    }
+                }
+            }
         }
 
         var isExpanded by remember { mutableStateOf(false) }
@@ -500,11 +613,95 @@ fun SettingScreen(
     }
 }
 
+//@Composable
+//@Preview(showBackground = true)
+//fun PreviewSettingScreen() {
+//    SettingScreen(
+//        navHostController = NavHostController(androidx.compose.ui.platform.LocalContext.current),
+//        context = androidx.compose.ui.platform.LocalContext.current
+//    )
+//}
+
+@Composable
+fun Dialog() {
+    Dialog(onDismissRequest = { }) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp),
+            shape = RoundedCornerShape(10.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                verticalArrangement = Arrangement.Center,
+//                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Sign out",
+                    style = TextStyle(
+                        fontSize = 40.sp,
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold
+                    ),
+                    textAlign = TextAlign.Left,
+                )
+                SpacerHeight(10.dp)
+                Text(
+                    text = "Are you sure you want to sign out?",
+                    style = TextStyle(
+                        fontSize = 15.sp,
+                        color = Color.Gray,
+                        fontWeight = FontWeight.Bold,
+                    ),
+                    textAlign = TextAlign.Left,
+                )
+//                SpacerHeight(10.dp)
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 10.dp)
+                        .weight(1f),
+                    horizontalArrangement = Arrangement.End,
+                    verticalAlignment = Alignment.Bottom
+                ) {
+                    Text(
+                        text = "Cancel",
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            color = Color.Gray,
+                            fontWeight = FontWeight.Bold
+                        ),
+                        textAlign = TextAlign.Left,
+                        modifier = Modifier
+                            .clickable {
+//                                openSignoutLog = false
+                            }
+                    )
+                    SpacerWidth(10.dp)
+                    Text(
+                        text = "Sign out",
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            color = Color.Red,
+                            fontWeight = FontWeight.Bold
+                        ),
+                        textAlign = TextAlign.Left,
+                        modifier = Modifier
+                            .clickable {
+//                                openSignoutLog = false
+//                                navHostController.navigate("login")
+                            }
+                    )
+                }
+            }
+        }
+    }
+}
+
 @Composable
 @Preview(showBackground = true)
-fun PreviewSettingScreen() {
-    SettingScreen(
-        navHostController = NavHostController(androidx.compose.ui.platform.LocalContext.current),
-        context = androidx.compose.ui.platform.LocalContext.current
-    )
+fun PreviewDialog() {
+    Dialog()
 }
